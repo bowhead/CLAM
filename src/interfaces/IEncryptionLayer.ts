de46e6e835887133b7a8fe1849ea5a8f84705939
TorroSoft <torrozapata@gmail.com>
2022-07-22 08:10:09 -0700

    The IEncryptionLayer interface was modified

--- src/interfaces/IEncryptionLayer.ts
+++ src/interfaces/IEncryptionLayer.ts
@@ -1,38 +1,25 @@
-import IPGPKeys from "./IPGPKeys";
-
 /**
- * This interface represents the implementation for generating 
- * the private and public keys using PGP (Pretty Good Privacy) 
- * and the encryptData and decryptData methods using the public 
- * and private keys.
+ * This interface represents the implementation for 
+ * encrypting and decrypting data using different encryption and decryption algorithms.
  */
 interface IEncryptionLayer {
-
-    /**
-     * This function generates the public and private pgp keys.
-     * 
-     * @param name This parameter is the name of the user that will generate the public and private pgp keys.
-     * @param email This parameter is the email of the user that will generate the public and private pgp keys.
-     */
-    generatePGPKeys(name: string, email: string): Promise<IPGPKeys>;
-
     /**
      * This function encrypts the data passed as a parameter using the 
-     * public key passed as a parameter.
+     * key passed as a parameter.
      * 
-     * @param publicKeyPGP This parameter is the public key to encrypt the data.
+     * @param key This parameter is the key to encrypt the data.
      * @param data This parameter is the data that will be encrypted.
      */
-    ecryptData(publicKeyPGP: string, data: any): Promise<string>;
+    ecryptData(key: string, data: any): Promise<string>;
 
     /**
      * This function decrypt the data passed as a parameter using the
-     * private key passed as a parameter.
+     * key passed as a parameter.
      * 
-     * @param privateKeyPGP This parameter is the private key to decrypt the data.
-     * @param dataEncrypted This parameter is the encrypted data that will be decrypted.
+     * @param key This parameter is the key to decrypt the data.
+     * @param data This parameter is the encrypted data that will be decrypted.
      */
-    decryptData(privateKeyPGP: any, dataEncrypted: any): Promise<string>;
+    decryptData(key: any, data: any): Promise<string>;
 }
 
 export default IEncryptionLayer;
