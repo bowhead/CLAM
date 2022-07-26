fa97c525275b3a4acf7f44a21bfdcfe6a85df813
TorroSoft <torrozapata@gmail.com>
2022-07-26 13:53:59 -0700

    Consent class was added

--- src/contractInteraction/consentInteraction/Consent.ts
+++ src/contractInteraction/consentInteraction/Consent.ts
@@ -1,4 +1,23 @@
-class Consent{
+class Consent {
+    consented: boolean;
+    consentDate: Date;
+    address: string;
+    addressConsentIndex: Map<string, number>;
+    publicPGPKeys: Map<string, string>;
+
+    constructor(
+        consented: boolean,
+        consentDate: Date,
+        address: string,
+        addressConsentIndex: Map<string, number>,
+        publicPGPKeys: Map<string, string>
+    ) {
+        this.consented = consented;
+        this.consentDate = consentDate;
+        this.address = address;
+        this.addressConsentIndex = addressConsentIndex;
+        this.publicPGPKeys = publicPGPKeys;
+    }
 
 }
 
