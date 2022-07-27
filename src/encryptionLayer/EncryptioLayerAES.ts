110f9cc6b8c11ab33d3d94191e0cd24a15f09e2a
TorroSoft <torrozapata@gmail.com>
2022-07-27 09:56:45 -0700

    EncryptionLayer components was modified

--- src/encryptionLayer/EncryptioLayerAES.ts
+++ src/encryptionLayer/EncryptioLayerAES.ts
@@ -6,7 +6,15 @@ class EncryptioLayerAES implements IEncryptionLayer {
     private keySize: number = 256;
     private iterations: number = 100;
 
-    ecryptData(key: string, data: any): Promise<string> {
+    /**
+     * This function encrypts the data passed as a parameter 
+     * using the key passed as a parameter with the AES-25 algorithm.
+     * 
+     * @param key This parameter is the key with which the information will be encrypted.
+     * @param data This parameter is the information to be encrypted.
+     * @returns returns a string promise, when resolved it returns a string representing the encrypted data.
+     */
+    ecryptData(key: string, data: string): Promise<string> {
         const salt = CryptoJS.lib.WordArray.random(128 / 8);
         const keyEncrypt = CryptoJS.PBKDF2(key, salt, {
             keySize: this.keySize / 32,
@@ -19,11 +27,22 @@ class EncryptioLayerAES implements IEncryptionLayer {
             mode: CryptoJS.mode.CBC,
         });
         const transitmessage = salt.toString() + iv.toString() + encrypted.toString();
+        console.log(transitmessage);
+        
         return transitmessage;
     }
-    decryptData(key: any, data: any): Promise<string> {
+    /**
+     * This function decrypt the data passed as a perameter
+     * using the key passed as a parameter.
+     * 
+     * @param key This parameter is the key which the information will be decrypted.
+     * @param data This paramater is the data encrypted to be decrypted.
+     * @returns return a string promise, when resolve it returns a string representing the decrypted data.
+     */
+    decryptData(key: any, data: string): Promise<string> {
         const salt = CryptoJS.enc.Hex.parse(data.substr(0, 32));
         const iv = CryptoJS.enc.Hex.parse(data.substr(32, 32));
+
         const encrypted = data.substring(64);
         const keyEncrypt = CryptoJS.PBKDF2(key, salt, {
             keySize: this.keySize / 32,
--- src/encryptionLayer/EncryptioLayerPGP.ts
+++ src/encryptionLayer/EncryptioLayerPGP.ts
@@ -1,12 +1,7 @@
 import IEncryptionLayer from "./IEncryptionLayer";
-import {readKey, encrypt, decrypt, readMessage, createMessage, decryptKey, readPrivateKey } from 'openpgp';
+import { readKey, encrypt, decrypt, readMessage, createMessage, decryptKey, readPrivateKey } from 'openpgp';
 
-/**
- * This class is used to generate public and private keys using PGP 
- * (pretty good privacy) as well as encrypt and decrypt data using 
- * the above mentioned standard together with public and private keys.
- */
-class EncryptionLayer implements IEncryptionLayer {
+class EncryptionLayerPGP implements IEncryptionLayer {
     /**
      * This function encrypts the data passed as a parameter using the 
      * public key passed as a parameter.
@@ -14,17 +9,13 @@ class EncryptionLayer implements IEncryptionLayer {
      * @param publicKeyPGP This parameter is the public key to encrypt the data.
      * @param data This parameter is the data that will be encrypted.
      */
-    ecryptData = async (publicKeyPGP: string, data: any): Promise<string> => {
-        try {
-            const publicKey = await readKey({ armoredKey: publicKeyPGP });
-            const encrypted = await encrypt({
-                message: await createMessage({ text: data }),
-                encryptionKeys: publicKey,
-            });
-            return encrypted.toString();
-        } catch (error) {
-            return "Error while encrypting data";
-        }
+    ecryptData = async (publicKeyPGP: string, data: string): Promise<string> => {
+        const publicKey = await readKey({ armoredKey: publicKeyPGP });
+        const encrypted = await encrypt({
+            message: await createMessage({ text: data }),
+            encryptionKeys: publicKey,
+        });
+        return encrypted.toString();
     }
 
     /**
@@ -34,24 +25,20 @@ class EncryptionLayer implements IEncryptionLayer {
      * @param privateKeyPGP This parameter is the private key to decrypt the data.
      * @param dataEncrypted This parameter is the encrypted data that will be decrypted.
      */
-    decryptData = async (privateKeyPGP: any, dataEncrypted: any): Promise<string> => {
-        try {
-            const privateKey = await decryptKey({
-                privateKey: await readPrivateKey({ armoredKey: privateKeyPGP }),
-                passphrase: "passphrase"
-            });
-            const message = await readMessage({
-                armoredMessage: dataEncrypted
-            });
-            const { data: decrypted } = await decrypt({
-                message,
-                decryptionKeys: privateKey
-            });
-            return decrypted.toString();
-        } catch (error) {
-            return "Error while decrypting data";
-        }
+    decryptData = async (privateKeyPGP: string, dataEncrypted: string): Promise<string> => {
+        const privateKey = await decryptKey({
+            privateKey: await readPrivateKey({ armoredKey: privateKeyPGP }),
+            passphrase: "passphrase"
+        });
+        const message = await readMessage({
+            armoredMessage: dataEncrypted
+        });
+        const { data: decrypted } = await decrypt({
+            message,
+            decryptionKeys: privateKey
+        });
+        return decrypted.toString();
     }
 };
 
-export default EncryptionLayer;
+export default EncryptionLayerPGP;
