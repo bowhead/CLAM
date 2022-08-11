b307787f6d20f210bb9e4c13c2e18e3deb88c322
TorroSoft <torrozapata@gmail.com>
2022-08-11 10:14:08 -0700

    StorageEngine was modified with the eslint rules

--- src/storageEngine/IStorageEngine.ts
+++ src/storageEngine/IStorageEngine.ts
@@ -1,27 +1,27 @@
 interface IStorageEngine {
     /**
      * Save file in a specific storage engine
-     * @param options - File to save and additional parameters
-     * @returns returns the file identifier or location
+     * @param {any} options - File to save and additional parameters
+     * @returns {Promise<string>} returns the file identifier or location
      */
     saveFile(options: any): Promise<string>;
 
     /**
      * Get file from storage engine
-     * @param options - File identifier or location and additional parameters
-     * @returns returns the file
+     * @param {any} options - File identifier or location and additional parameters
+     * @returns {Promise<string>} returns the file
      */
     getFile(options: any): Promise<string>;
-
+    
     /**
      * Update file stored
-     * @param options - File identifier or location and additional parameters
+     * @param {any} options - File identifier or location and additional parameters
      */
     updateFile(options: any): Promise<void>;
 
     /**
      * Delete file from storage engine
-     * @param options - Identifier of the file to delete and additional parameters
+     * @param {any} options - Identifier of the file to delete and additional parameters
      */
     deleteFile(options: any): Promise<void>;
 }
