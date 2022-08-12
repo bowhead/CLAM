6086904be70349de4bfeb45e1ecf05b683bd77f2
TorroSoft <torrozapata@gmail.com>
2022-08-12 08:48:36 -0700

    ContractInteration was modified with eslint rules

--- src/contractIntegration/interaction/FactoryInteraction.ts
+++ src/contractIntegration/interaction/FactoryInteraction.ts
@@ -1,57 +1,82 @@
-import "reflect-metadata"
-import { container } from "tsyringe";
+import 'reflect-metadata';
+import { container } from 'tsyringe';
 import {
     ConsentInteraction,
     AccessInteraction,
     Interaction,
     IOption
-} from "../";
-
+} from '../';
+/**
+ * This class is used to generate 
+ */
 class FactoryInteraction {
     public optionsConsentInteraction: IOption[];
     public optionsAccessInteraction: IOption[];
+    /**
+     * 
+     */
     public constructor() {
-        this.optionsConsentInteraction = [{ name: "clam", option: ConsentInteraction }]
-        this.optionsAccessInteraction = [{ name: "clam", option: AccessInteraction }]
+        this.optionsConsentInteraction = [{ name: 'clam', option: ConsentInteraction }];
+        this.optionsAccessInteraction = [{ name: 'clam', option: AccessInteraction }];
     }
+    /**
+     * This function set a new implementation of ConsentInteraction.
+     * 
+     * @param {IOption} option This parameter contains the name and class of the new implementation.
+     */
     public setOptionConsentInteraction(option: IOption): void {
-        if(option.name.trim()==="" && option.name.trim().length===0){
-            throw new Error("The name must have at least one character");
+        if(option.name.trim()==='' && option.name.trim().length===0){
+            throw new Error('The name must have at least one character');
         }
         const optionConsentExist = this.optionsConsentInteraction.find(optionAux => optionAux.name === option.name);
         if (optionConsentExist) {
-            throw new Error("This option already exists.");
+            throw new Error('This option already exists.');
         }
         this.optionsConsentInteraction.push(option);
     }
+
+    /**
+     * This function set a new implementation of AccessInteraction.
+     * 
+     * @param {IOption} option This parameter contains the name and class of the new implementation.
+     */
     public setOptionAccessInteraction(option: IOption): void {
-        if(option.name.trim()==="" && option.name.trim().length===0){
-            throw new Error("The name must have at least one character");
+        if(option.name.trim()==='' && option.name.trim().length===0){
+            throw new Error('The name must have at least one character');
         }
         const optionAccessExist = this.optionsAccessInteraction.find(optionAux => optionAux.name === option.name);
         if (optionAccessExist) {
-            throw new Error("This option already exists.");
+            throw new Error('This option already exists.');
         }
         this.optionsAccessInteraction.push(option);
     }
+    
+    /**
+     * This function generate a new instance of Interaction class using the implementations
+     * passed in the parameter.
+     * 
+     * @param {string} consentType This parameter is the option of consenInteraction implementation.
+     * @param {string} accessType This parameter is the option of accessInteraction Implementation.
+     * @returns {Interaction} return a new instance of Interaction class.
+     */
     public generateInteraction(consentType: string, accessType: string): Interaction {
-        if(consentType.trim()==="" && consentType.trim().length===0){
-            throw new Error("The consent implementation name must have a minimum of one character");
+        if(consentType.trim()==='' && consentType.trim().length===0){
+            throw new Error('The consent implementation name must have a minimum of one character');
         }
-        if(accessType.trim()==="" && accessType.trim().length===0){
-            throw new Error("The access implementation name must have a minimum of one character");
+        if(accessType.trim()==='' && accessType.trim().length===0){
+            throw new Error('The access implementation name must have a minimum of one character');
         }
         const consent = this.optionsConsentInteraction.find(option => option.name.toLowerCase() === consentType.toLowerCase());
         const access = this.optionsAccessInteraction.find(option => option.name.toLowerCase() === accessType.toLowerCase());
 
         if (!consent) {
-            throw new Error("The consentInteraction type doesn't exist");
+            throw new Error('The consentInteraction type doesn\'t exist');
         }
         if (!access) {
-            throw new Error("The accessInteraction type doesn't exist");
+            throw new Error('The accessInteraction type doesn\'t exist');
         }
-        container.register("ConsentInteraction", consent.option);
-        container.register("AccessInteraction", access.option);
+        container.register('ConsentInteraction', consent.option);
+        container.register('AccessInteraction', access.option);
 
         const interaction: Interaction = container.resolve(Interaction);
 
--- src/contractIntegration/interaction/Interaction.ts
+++ src/contractIntegration/interaction/Interaction.ts
@@ -1,9 +1,14 @@
-import { injectable, inject } from "tsyringe";
+import { injectable, inject } from 'tsyringe';
 import {
     IConsentInteraction,
     IAccessInteraction,
-} from "../";
-import { IdentityManager } from "../../";
+} from '../';
+import { IdentityManager } from '../../';
+
+/**
+ * This class is used to interact with the blockchain by making use 
+ * of consent and access interaction instances.
+ */
 @injectable()
 class Interaction {
 
@@ -12,16 +17,35 @@ class Interaction {
     public urlProvider: string;
     public identity: IdentityManager;
 
+    /**
+     * This constructor initializes the intent by injecting a specific 
+     * implementation of consent and access.
+     * 
+     * @param {IConsentInteraction} consentInteraction This parameter is the implementation of ConsentInteraction.
+     * @param {IAccessInteraction} acccessInteraction This parameter is the implementation of AccessInteration.
+     */
     public constructor(
-        @inject("ConsentInteraction") consentInteraction: IConsentInteraction,
-        @inject("AccessInteraction") acccessInteraction: IAccessInteraction
+        @inject('ConsentInteraction') consentInteraction: IConsentInteraction,
+        @inject('AccessInteraction') acccessInteraction: IAccessInteraction
     ) {
         this.acccessInteraction = acccessInteraction;
         this.consentInteraction = consentInteraction;
     }
+    /**
+     * This function set a new URL provider of web 3
+     * 
+     * @param {string} urlProvider This parameter is the url provider.
+     */
     public setUrlProvider(urlProvider: string): void {
         this.urlProvider = urlProvider;
     }
+
+    /**
+     * This function establishes a new identity to 
+     * perform the operations with the web 3
+     * 
+     * @param {IdentityManager} identity This parameter is the identity that will be used to interact with the web.
+     */
     public setIdentity(identity: IdentityManager): void {
         this.identity = identity;
     }
