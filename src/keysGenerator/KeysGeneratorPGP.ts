518c5e454b48cdbc7a52768a83766f1fa5d77f65
TorroSoft <torrozapata@gmail.com>
2022-08-11 21:02:41 -0700

    KeysGenerator was modified with eslint rules

--- src/keysGenerator/KeysGeneratorPGP.ts
+++ src/keysGenerator/KeysGeneratorPGP.ts
@@ -1,21 +1,31 @@
-import { injectable } from "tsyringe";
-import IKeysGenerator from "./IKeysGenerator";
-import IKeys from "./IKeys";
-import { generateKey } from "openpgp";
+import { injectable } from 'tsyringe';
+import IKeysGenerator from './IKeysGenerator';
+import IKeys from './IKeys';
+import { generateKey } from 'openpgp';
+
+/**
+ * This class
+ */
 @injectable()
 class KeysGeneratorPGP implements IKeysGenerator {
 
+    /**
+     * This function generate keys using an specific implementation.
+     * 
+     * @param {any} data This parameter is the information to generate the keys.
+     * @returns {Primise<IKeys>} return the keys.
+     */
     public generateKeys = async (data: any): Promise<IKeys> => {
         const pgpKeys: IKeys = {
-            privateKey: "",
-            publicKey: ""
+            privateKey: '',
+            publicKey: ''
         };
         const { name, email } = data;
         const { privateKey, publicKey } = await generateKey({
             type: 'ecc',
             curve: 'curve25519',
             userIDs: [{ name, email }],
-            passphrase: "passphrase",
+            passphrase: 'passphrase',
             format: 'armored'
         });
         pgpKeys.privateKey = privateKey;
--- src/keysGenerator/index.ts
+++ src/keysGenerator/index.ts
@@ -1,9 +1,9 @@
-import KeysGeneratorPGP from "./KeysGeneratorPGP";
-import IKeysGenerator from "./IKeysGenerator";
-import IKeys from "./IKeys";
+import KeysGeneratorPGP from './KeysGeneratorPGP';
+import IKeysGenerator from './IKeysGenerator';
+import IKeys from './IKeys';
 
 export {
     IKeys,
     IKeysGenerator,
     KeysGeneratorPGP
-}
+};
