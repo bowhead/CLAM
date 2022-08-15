09c2e76421c4e388b1160128ea147ca424c5714a
TorroSoft <torrozapata@gmail.com>
2022-08-14 17:51:15 -0700

    Utilities was moved and the any's in consentInteraction were deleted

--- src/contractIntegration/consentInteraction/ConsentInteraction.ts
+++ src/contractIntegration/consentInteraction/ConsentInteraction.ts
@@ -5,6 +5,7 @@ import { IConsentInteraction } from '.';
 import Web3 from 'web3';
 import { IdentityManager } from '../../indentityManager';
 import Web3Provider from '../interaction/Wbe3Provider';
+import IConsentKeys from './IConsentKeys';
 /**
  * This class represent the implementation of IConsentInteraction interface,
  * this class is used to interact with the consent smart contract.
@@ -19,16 +20,16 @@ class ConsentInteraction implements IConsentInteraction {
      * @param {IdentityManager} identity This parameter is the Identity to configurate the smart contract interaction.
      * @returns {Promise<any>} return the address of the transaction.
      */
-    saveConsent(consentId: string, identity: IdentityManager): Promise<any> {
+    saveConsent(consentId: string, identity: IdentityManager): Promise<string> {
         if (consentId.trim() === '' || consentId.trim().length === 0) throw new Error('contentID must have at least 1 character');
 
         const objWeb3 = Web3Provider.getInstance().getProvider();
         const provider = Web3Provider.getInstance();
         const contract = new objWeb3.eth.Contract(provider.consentConfig.abi, provider.consentConfig.address, { from: identity.address });
         return new Promise((resolve, reject) => {
-            contract.methods.updateConsent(Web3.utils.fromAscii(consentId), true).send(function (error: any, result: any) {
+            contract.methods.updateConsent(Web3.utils.fromAscii(consentId), true).send(function (error: Error, result: string) {
                 if (!error) {
-                    resolve({ result });
+                    resolve(result);
                 }
                 else {
                     reject(error);
@@ -45,16 +46,16 @@ class ConsentInteraction implements IConsentInteraction {
      * @param {IdentityManager} identity This parameter is the Identity to configurate the smart contract interaction.
      * @returns {Promise<any>} return the address of the transaction.
      */
-    async cancelConsent(consentId: string, identity: IdentityManager): Promise<any> {
+    async cancelConsent(consentId: string, identity: IdentityManager): Promise<string> {
         if (consentId.trim() === '' || consentId.trim().length === 0) throw new Error('contentID must have at least 1 character');
 
         const objWeb3 = Web3Provider.getInstance().getProvider();
         const provider = Web3Provider.getInstance();
         const contract = new objWeb3.eth.Contract(provider.consentConfig.abi, provider.consentConfig.address, { from: identity.address });
         return new Promise((resolve, reject) => {
-            contract.methods.updateConsent(Web3.utils.fromAscii(consentId), false).send(function (error: any, result: any) {
+            contract.methods.updateConsent(Web3.utils.fromAscii(consentId), false).send(function (error: Error, result: string) {
                 if (!error) {
-                    resolve({ result });
+                    resolve(result);
                 }
                 else {
                     reject(error);
@@ -72,7 +73,7 @@ class ConsentInteraction implements IConsentInteraction {
      * @param {IdentityManager} identity This parameter is the Identity to configurate the smart contract interaction.
      * @returns {Promise<any>} return the consent status. 
      */
-    getConsentById(consentId: string, owner: string, identity: IdentityManager): Promise<any> {
+    getConsentById(consentId: string, owner: string, identity: IdentityManager): Promise<boolean> {
         if (consentId.trim() === '' || consentId.trim().length === 0) throw new Error('contentID must have at least 1 character');
         if (owner.trim() === '' || owner.trim().length === 0) throw new Error('Owner must have at least 1 character');
         if (!owner.trim().includes('0x')) throw new Error('Invalid owner, the string with has a correct format.');
@@ -81,9 +82,9 @@ class ConsentInteraction implements IConsentInteraction {
         const provider = Web3Provider.getInstance();
         const contract = new objWeb3.eth.Contract(provider.consentConfig.abi, provider.consentConfig.address, { from: identity.address });
         return new Promise((resolve, reject) => {
-            contract.methods.getConsent(Web3.utils.fromAscii(consentId), owner).call(function (error: any, result: any) {
+            contract.methods.getConsent(Web3.utils.fromAscii(consentId), owner).call(function (error: Error, result: boolean) {
                 if (!error) {
-                    resolve({ result });
+                    resolve(result);
                 }
                 else {
                     reject(error);
@@ -102,7 +103,7 @@ class ConsentInteraction implements IConsentInteraction {
      * @param {IdentityManager} identity This parameter is the Identity to configurate the smart contract interaction. 
      * @returns {Promise<any>}  return the address of the transaction.  
      */
-    addKey(consentId: string, addressConsent: string, key: string, identity: IdentityManager): Promise<any> {
+    addKey(consentId: string, addressConsent: string, key: string, identity: IdentityManager): Promise<string> {
         if (consentId.trim() === '' || consentId.trim().length === 0) throw new Error('contentID must have at least 1 character');
         if (addressConsent.trim() === '' || addressConsent.trim().length === 0) throw new Error('AddressConsent must have at least 1 character');
         if (!addressConsent.trim().includes('0x')) throw new Error('Invalid addressConsent, the string with has a correct format.');
@@ -112,9 +113,9 @@ class ConsentInteraction implements IConsentInteraction {
         const provider = Web3Provider.getInstance();
         const contract = new objWeb3.eth.Contract(provider.consentConfig.abi, provider.consentConfig.address, { from: identity.address });
         return new Promise((resolve, reject) => {
-            contract.methods.addPGPKey(Web3.utils.fromAscii(consentId), addressConsent, key).send({ gas: '1000000' }, function (error: any, result: any) {
+            contract.methods.addPGPKey(Web3.utils.fromAscii(consentId), addressConsent, key).send({ gas: '1000000' }, function (error: Error, result: string) {
                 if (!error) {
-                    resolve({ result });
+                    resolve(result);
                 }
                 else {
                     reject(error);
@@ -130,16 +131,16 @@ class ConsentInteraction implements IConsentInteraction {
      * @param {IdentityManager} identity This parameter is the Identity to configurate the smart contract interaction. 
      * @returns {Promise<any>} return the addres's and keys's
      */
-    getKeys(consentId: string, identity: IdentityManager): Promise<any> {
+    getKeys(consentId: string, identity: IdentityManager): Promise<IConsentKeys> {
         if (consentId.trim() === '' || consentId.trim().length === 0) throw new Error('contentID must have at least 1 character');
-
+        
         const objWeb3 = Web3Provider.getInstance().getProvider();
         const provider = Web3Provider.getInstance();
         const contract = new objWeb3.eth.Contract(provider.consentConfig.abi, provider.consentConfig.address, { from: identity.address });
         return new Promise((resolve, reject) => {
-            contract.methods.getPGPKeys(Web3.utils.fromAscii(consentId)).call(function (error: any, result: any) {
+            contract.methods.getPGPKeys(Web3.utils.fromAscii(consentId)).call(function (error: Error, result: IConsentKeys) {
                 if (!error) {
-                    resolve({ result });
+                    resolve(result);
                 }
                 else {
                     reject(error);
---
+++ src/contractIntegration/consentInteraction/IConsentKeys.ts
@@ -0,0 +1,6 @@
+interface IConsentKeys {
+    '0': string[];
+    '1': string[];
+}
+
+export default IConsentKeys;
