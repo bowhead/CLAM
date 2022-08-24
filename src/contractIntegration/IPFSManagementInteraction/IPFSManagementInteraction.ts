1ce509d1873a8f5ca1cd9e069f136957c003963a
Raul Ziranda <raul.ziranda@bowheadhealth.com>
2022-08-24 10:33:47 -0500

    Test IPFS transactions

--- src/contractIntegration/IPFSManagementInteraction/IPFSManagementInteraction.ts
+++ src/contractIntegration/IPFSManagementInteraction/IPFSManagementInteraction.ts
@@ -19,24 +19,16 @@ class IPFSManagementInteraction implements IIPFSManagementInteraction {
      * @param {IdentityManager} identity - Identity of the file owner
      * @returns void
      */
-    addFile(fileHash: string, fileName: string, identity: IdentityManager): Promise<void> {
+    async addFile(fileHash: string, fileName: string, identity: IdentityManager): Promise<void> {
         if (fileHash.trim() === '' || fileHash.trim().length === 0) throw new Error('fileHash must have at least 1 character');
         if (fileName.trim() === '' || fileName.trim().length === 0) throw new Error('fileName must have at least 1 character');
 
         const objWeb3 = Web3Provider.getInstance().getProvider();
         const provider = Web3Provider.getInstance();
         const contract = new objWeb3.eth.Contract(provider.IPFSManagementConfig.abi, provider.IPFSManagementConfig.address, { from: identity.address });
-
-        return new Promise((resolve, reject) => {
-            contract.methods.addFile(fileHash, Web3.utils.fromAscii(fileName)).send(function (error: Error) {
-                if (!error) {
-                    resolve();
-                }
-                else {
-                    reject(error);
-                }
-            });
-        });
+        const transaction =  contract.methods.addFile(fileHash, Web3.utils.fromAscii(fileName));
+        await this.send(transaction, objWeb3, identity);
+        return;
     }
 
     /**
@@ -45,23 +37,16 @@ class IPFSManagementInteraction implements IIPFSManagementInteraction {
      * @param {IdentityManager} identity - Identity of the file owner
      * @returns void
      */
-    removeFile(fileHash: string, identity: IdentityManager): Promise<void> {
+    async removeFile(fileHash: string, identity: IdentityManager): Promise<void> {
         if (fileHash.trim() === '' || fileHash.trim().length === 0) throw new Error('fileHash must have at least 1 character');
 
         const objWeb3 = Web3Provider.getInstance().getProvider();
         const provider = Web3Provider.getInstance();
         const contract = new objWeb3.eth.Contract(provider.IPFSManagementConfig.abi, provider.IPFSManagementConfig.address, { from: identity.address });
 
-        return new Promise((resolve, reject) => {
-            contract.methods.removeFile(fileHash).send(function (error: Error) {
-                if (!error) {
-                    resolve();
-                }
-                else {
-                    reject(error);
-                }
-            });
-        });
+        const transaction = contract.methods.removeFile(fileHash);
+        await this.send(transaction, objWeb3, identity);
+        return
     }
 
     /**
@@ -133,6 +118,26 @@ class IPFSManagementInteraction implements IIPFSManagementInteraction {
                 }
             });
         });
+    }
+
+    /**
+     * This function sign the transaction.
+     * 
+     * @param {any} transaction This parameter is the transaction object. 
+     * @param {Web3} web3 This parameter is the Web3 Provider to sign the transaction. 
+     * @param {IdentityManager} identity This parameter is the identity to sign the transaction with it's privateKey. 
+     * @returns {Promise<boolean>} Return true if the transaction was successful, false otherwise.
+     */
+     private async send(transaction: any, web3: Web3, identity: IdentityManager): Promise<boolean> {
+        const options = {
+            to: transaction._parent._address,
+            data: transaction.encodeABI(),
+            gas: await transaction.estimateGas({ from: identity.address }),
+            gasPrice: web3.utils.toHex(web3.utils.toWei('30', 'gwei'))
+        };
+        const signed = await web3.eth.accounts.signTransaction(options, identity.privateKey);
+        const receipt = await web3.eth.sendSignedTransaction(signed.rawTransaction as string);
+        return receipt.status;
     }
 }
 
