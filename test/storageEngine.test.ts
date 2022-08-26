711ab1a544e72d4f34ab1c06382580e1414a9c6b
HectorGerardoZV <torrozapata@gmail.com>
2022-08-25 23:22:00 -0700

    StorageEngine test was modified to use toThrow and remove the trycatch that conflicted with the use of 'any'.

--- test/storageEngine.test.ts
+++ test/storageEngine.test.ts
@@ -13,24 +13,19 @@ describe('Testing storage engine using IPFS service as default', () => {
     let cid: string;
     const address = '0x7EEc887Ff77e28D7Cbd2057E1da4251F48B81336';
     const privateKey = 'bebefc9fd249df72a5b010e92adac9353ea11cc5825e5c710ef2da831e948c74';
-
     test('Should add new file', async () => {
         nock('http://localhost:3000')
             .post('/file')
             .reply(200, {
                 CID: 'fe5c3e7fa0f43b8cbfed5e69c9a19c722c1900ff893ce7fa6b40646b88e46f48.txt'
             })
-
         const options = {
             file: fs.createReadStream(path.resolve(__dirname, './resources/test.txt')),
             address: address,
             fileName: 'test.txt'
         };
-
         cid = await storageEngine.saveFile(options);
-
         // await ipfsManagement.addFile(cid, options.fileName);
-
         expect(cid).not.toBe('');
     });
 
@@ -39,16 +34,13 @@ describe('Testing storage engine using IPFS service as default', () => {
             address: address,
             cid: cid
         };
-
         nock('http://localhost:3000')
             .get('/file')
             .query({ address: address, cid: cid })
             .reply(200, {
                 file: 'dGVzdHYxMA=='
             })
-
         const file = await storageEngine.getFile(options);
-
         expect(Buffer.from(file, 'base64').toString()).toBe('testv10');
     });
 
@@ -59,35 +51,27 @@ describe('Testing storage engine using IPFS service as default', () => {
             cid: cid,
             privateKey: privateKey
         };
-
         nock('http://localhost:3000')
             .get('/challenge')
             .query({ address: address })
             .reply(200, {
                 'hash': '71baf499ea88cf4c4cf06b9480e48ffae11e987e49f0d6a6c7061f4f02a4b0d2'
             });
-
         nock('http://localhost:3000')
             .put('/file')
             .reply(200)
-
         await storageEngine.updateFile(options);
-
         const getOptions = {
             address: address,
             cid: cid
         };
-
-
         nock('http://localhost:3000')
             .get('/file')
             .query({ address: address, cid: cid })
             .reply(200, {
                 file: 'dGVzdHYxMQ=='
             })
-
         const file = await storageEngine.getFile(getOptions);
-
         expect(Buffer.from(file, 'base64').toString()).toBe('testv11');
     });
 
@@ -97,98 +81,74 @@ describe('Testing storage engine using IPFS service as default', () => {
             cid: cid,
             privateKey: privateKey
         };
-
-
         nock('http://localhost:3000')
             .get('/challenge')
             .query({ address: address })
             .reply(200, {
                 'hash': '71baf499ea88cf4c4cf06b9480e48ffae11e987e49f0d6a6c7061f4f02a4b0d2'
             });
-
         nock('http://localhost:3000')
             .delete('/file')
             .reply(200)
-
         await storageEngine.deleteFile(options);
-
         const getOptions = {
             address: address,
             cid: cid
         };
-
-
         nock('http://localhost:3000')
             .get('/file')
             .query({ address: address, cid: cid })
             .reply(200, {
                 file: ''
             })
-
         const file = await storageEngine.getFile(getOptions);
-
         expect(file).toBe('');
     });
 
     test('Should not update file when the file not exist', async () => {
-        try {
+        await expect(async () => {
             const options = {
                 file: fs.createReadStream(path.resolve(__dirname, './resources/testUpdate.txt')),
                 address: address,
                 cid: cid,
                 privateKey: privateKey
             };
-
             nock('http://localhost:3000')
                 .get('/challenge')
                 .query({ address: address })
                 .reply(200, {
                     'hash': '71baf499ea88cf4c4cf06b9480e48ffae11e987e49f0d6a6c7061f4f02a4b0d2'
                 });
-
             nock('http://localhost:3000')
                 .put('/file')
                 .replyWithError({
                     code: 404,
                     message: 'File not found'
                 });
-
             await storageEngine.updateFile(options);
-        } catch (error) {
-            expect(error).toBeInstanceOf(Error);
-            expect(error.response.status).toBe(404);
-            expect(error.response.data.message).toBe('File not found');
-        }
+        }).rejects.toThrow('File not found');
     });
 
     test('Should not delete file when the file not exist', async () => {
-        try {
+        await expect(async () => {
             const options = {
                 address: address,
                 cid: cid,
                 privateKey: privateKey
-
             }
-
             nock('http://localhost:3000')
                 .get('/challenge')
                 .query({ address: address })
                 .reply(200, {
                     'hash': '71baf499ea88cf4c4cf06b9480e48ffae11e987e49f0d6a6c7061f4f02a4b0d2'
                 });
-
             nock('http://localhost:3000')
                 .delete('/file')
                 .replyWithError({
                     code: 404,
                     message: 'File not found'
                 });
-
             await storageEngine.deleteFile(options);
-        } catch (error) {
-            expect(error).toBeInstanceOf(Error);
-            expect(error.response.status).toBe(404);
-            expect(error.response.data.message).toBe('File not found');
-        }
+        }).rejects.toThrow('File not found');
     });
 });
