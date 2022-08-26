2d4e506b4166b35e2271d45497fe2a874c09ecef
HectorGerardoZV <torrozapata@gmail.com>
2022-08-26 00:20:32 -0700

    FactoryInteraction test was modified to use toThrow and remove the trycatch that conflicted with the use of 'any'.

--- test/factoryInteraction.test.ts
+++ test/factoryInteraction.test.ts
@@ -133,125 +133,101 @@ describe('Testing interaction component using the CLAM implementation', () => {
         const keys = Object.keys(factoryInteraction);
         expect(keys.includes('optionsConsentInteraction')).toBe(true);
         expect(keys.includes('optionsAccessInteraction')).toBe(true);
-        expect(keys.length).toBe(2);
+        expect(keys.includes('optionsIPFSManagementInteraction')).toBe(true);
+        expect(keys.length).toBe(3);
     });
+
     test('should generate an interaction with the clam implementations', () => {
         const factoryInteraction: FactoryInteraction = new FactoryInteraction();
         const interaction = factoryInteraction.generateInteraction('clam', 'clam', 'clam');
         const keys = Object.keys(interaction);
         expect(keys.includes('acccessInteraction')).toBe(true);
         expect(keys.includes('consentInteraction')).toBe(true);
-
-        expect(keys.length).toBe(2);
+        expect(keys.includes('IPFSManagementInteraction')).toBe(true);
+        expect(keys.length).toBe(3);
     });
-    test('should not generate an interaction if we dont specifie the consentInteraction type', () => {
-        try {
+
+    test('should not generate an interaction if we dont specifie the consentInteraction type', async () => {
+        await expect(async () => {
             const factoryInteraction: FactoryInteraction = new FactoryInteraction();
             factoryInteraction.generateInteraction('not', 'clam', 'clam');
-        } catch (error) {
-            expect(error).toBeInstanceOf(Error);
-            expect(error.message).toBe('The consentInteraction type doesn\'t exist');
-
-        }
+        }).rejects.toThrow(`The consentInteraction type doesn\'t exist`);
     });
-    test('should not generate an interaction if we dont specifie the accessInteraction type', () => {
-        try {
+
+    test('should not generate an interaction if we dont specifie the accessInteraction type', async () => {
+        await expect(async () => {
             const factoryInteraction: FactoryInteraction = new FactoryInteraction();
             factoryInteraction.generateInteraction('clam', 'not', 'clam');
-        } catch (error) {
-            expect(error).toBeInstanceOf(Error);
-            expect(error.message).toBe('The accessInteraction type doesn\'t exist');
-
-        }
+        }).rejects.toThrow('The accessInteraction type doesn\'t exist');
     });
+
     test('should add a new implementation to the FactoryInteraction', () => {
         const factoryInteraction: FactoryInteraction = new FactoryInteraction();
         factoryInteraction.setOptionConsentInteraction({ name: 'other', option: ConsentInteractionOther });
-
-
     });
+
     test('should generate an instance of interaction based on the new consent implementation.', () => {
         const factoryInteraction: FactoryInteraction = new FactoryInteraction();
         factoryInteraction.setOptionConsentInteraction({ name: 'other', option: ConsentInteractionOther });
         const interactionOther = factoryInteraction.generateInteraction('other', 'clam', 'clam');
-
         expect(interactionOther).not.toBe(null);
     });
+
     test('should generate an instance of interaction based on the new access implementation.', () => {
         const factoryInteraction: FactoryInteraction = new FactoryInteraction();
         factoryInteraction.setOptionAccessInteraction({ name: 'other', option: AccessInteractionOther });
         const interactionOther = factoryInteraction.generateInteraction('clam', 'other', 'clam');
-
         expect(interactionOther).not.toBe(null);
     });
+
     test('should generate an instance of interaction based on the new access and consent implementations.', () => {
         const factoryInteraction: FactoryInteraction = new FactoryInteraction();
         factoryInteraction.setOptionAccessInteraction({ name: 'other', option: AccessInteractionOther });
         factoryInteraction.setOptionConsentInteraction({ name: 'other', option: ConsentInteractionOther });
         const interactionOther = factoryInteraction.generateInteraction('other', 'other', 'clam');
-
         expect(interactionOther).not.toBe(null);
     });
 
-
-    test('should throw an error if we want to add an existing implementation in consent. ', () => {
-        try {
+    test('should throw an error if we want to add an existing implementation in consent. ', async () => {
+        await expect(async () => {
             const factoryInteraction: FactoryInteraction = new FactoryInteraction();
             factoryInteraction.setOptionConsentInteraction({ name: 'clam', option: ConsentInteractionOther });
-        } catch (error) {
-            expect(error).toBeInstanceOf(Error);
-            expect(error.message).toBe('This option already exists.');
-
-        }
+        }).rejects.toThrow('This option already exists.');
     });
-    test('should throw an error if we want to add an existing implementation in access. ', () => {
-        try {
+
+    test('should throw an error if we want to add an existing implementation in access. ', async () => {
+        await expect(async () => {
             const factoryInteraction: FactoryInteraction = new FactoryInteraction();
             factoryInteraction.setOptionAccessInteraction({ name: 'clam', option: AccessInteractionOther });
-        } catch (error) {
-            expect(error).toBeInstanceOf(Error);
-            expect(error.message).toBe('This option already exists.');
-
-        }
+        }).rejects.toThrow('This option already exists.');
     });
-    test('should throw an error if we want to add a new implementation of consent with the name empty ', () => {
-        try {
+
+    test('should throw an error if we want to add a new implementation of consent with the name empty ', async () => {
+        await expect(async () => {
             const factoryInteraction: FactoryInteraction = new FactoryInteraction();
             factoryInteraction.setOptionConsentInteraction({ name: '', option: ConsentInteractionOther });
-        } catch (error) {
-            expect(error).toBeInstanceOf(Error);
-            expect(error.message).toBe('The name must have at least one character');
-
-        }
+        }).rejects.toThrow('The name must have at least one character');
     });
-    test('should throw an error if we want to add a new implementation of access with the name empty ', () => {
-        try {
+
+    test('should throw an error if we want to add a new implementation of access with the name empty ', async () => {
+        await expect(async () => {
             const factoryInteraction: FactoryInteraction = new FactoryInteraction();
             factoryInteraction.setOptionAccessInteraction({ name: '', option: AccessInteractionOther });
-        } catch (error) {
-            expect(error).toBeInstanceOf(Error);
-            expect(error.message).toBe('The name must have at least one character');
-
-        }
+        }).rejects.toThrow('The name must have at least one character');
     });
-    test('should throw an error if we want to generate an interaction without consent type ', () => {
-        try {
+
+    test('should throw an error if we want to generate an interaction without consent type ', async () => {
+        await expect(async () => {
             const factoryInteraction: FactoryInteraction = new FactoryInteraction();
             factoryInteraction.generateInteraction('', 'clam', 'clam');
-        } catch (error) {
-            expect(error).toBeInstanceOf(Error);
-            expect(error.message).toBe('The consent implementation name must have a minimum of one character');
-
-        }
+        }).rejects.toThrow('The consent implementation name must have a minimum of one character');
     });
-    test('should throw an error if we want to generate an interaction without access type ', () => {
-        try {
+
+    test('should throw an error if we want to generate an interaction without access type ', async () => {
+        await expect(async () => {
             const factoryInteraction: FactoryInteraction = new FactoryInteraction();
             factoryInteraction.generateInteraction('clam', '', 'clam');
-        } catch (error) {
-            expect(error).toBeInstanceOf(Error);
-            expect(error.message).toBe('The access implementation name must have a minimum of one character');
-        }
+        }).rejects.toThrow('The access implementation name must have a minimum of one character');
     });
 });
 
